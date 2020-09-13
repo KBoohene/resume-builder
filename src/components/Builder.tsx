fea9f8ad4cec76ed0d089702d6b6740fd8e970cf
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-13 12:09:11 -0700

    Continuous basic file setup

---
+++ src/components/Builder.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Builder = () => {
+
+  const [state, setState] = useState();
+
+  return (
+    <div>
+      <div className="resume-builder">
+
+      </div>
+    </div>);
+}
+
+export default Builder;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Builder from './components/Builder';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Builder />,
   document.getElementById('root')
 );
