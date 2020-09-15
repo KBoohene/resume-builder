2f852cf093d57fd6a6e8042936b3a84deb72376a
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-15 01:33:17 -0700

    Add simple builder context

---
+++ src/components/Builder/BuilderContext.tsx
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react';
+type Props = {
+  children: React.ReactNode
+};
+
+const defaultTheme: string = "white";
+export const BuilderContext = createContext<string>(defaultTheme);
+
+const BuilderContextProvider = ({ children }: Props) => {
+  return (
+    <BuilderContext.Provider value={defaultTheme}>
+      {children}
+    </BuilderContext.Provider>
+  );
+}
+
+export default BuilderContextProvider;
