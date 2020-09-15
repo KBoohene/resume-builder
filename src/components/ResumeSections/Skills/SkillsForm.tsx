c413372c19fb64d0513d585218606b8a971d2944
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-15 00:23:25 -0700

    Add Input boxes to skills form

--- src/components/ResumeSections/Skills/SkillsForm.tsx
+++ src/components/ResumeSections/Skills/SkillsForm.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Input from '../../Shared/Input';
+import { InputType } from '../../Shared/Types'
 
 const SkillsForm = () => {
-  return (<div></div>);
+  return (
+    <div>
+      <h2>Skills</h2>
+      <Input type={InputType.text} label={''} value={''} placeholder={''} />
+      <Input type={InputType.text} label={''} value={''} placeholder={''} />
+      <Input type={InputType.text} label={''} value={''} placeholder={''} />
+      <Input type={InputType.text} label={''} value={''} placeholder={''} />
+      <Input type={InputType.text} label={''} value={''} placeholder={''} />
+    </div>
+  );
 }
 
 export default SkillsForm;
