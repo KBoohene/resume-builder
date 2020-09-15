75f80b93d396d3aef4a3726d73e70a8c51e08b50
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-14 23:45:47 -0700

    Add switch for rendering  resume sections

--- src/components/ResumeSections/ResumeSections.tsx
+++ src/components/ResumeSections/ResumeSections.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { SectionInterface, SectionType } from './Types';
+import EducationForm from './Education/EducationForm';
+import AchievementsForm from './Achievements/AchievementsForm';
 import ProfileForm from './Profile/ProfileForm';
+import ProjectsForm from './Projects/ProjectsForm';
+import SkillsForm from './Skills/SkillsForm';
+import RelevantCoursesForm from './RelevantCourses/RelevantCoursesForm';
 
-const ResumeSections = () => {
-
-  return (<div>
-    <ProfileForm />
-  </div>);
+const ResumeSections = ({ section }: SectionInterface) => {
+  return (
+    <div>
+      {(section === SectionType.profile) && (
+        <ProfileForm />
+      )}
+      {(section === SectionType.education) && (
+        <EducationForm />
+      )}
+      {(section === SectionType.projects) && (
+        <ProjectsForm />
+      )}
+      {(section === SectionType.achievements) && (
+        <AchievementsForm />
+      )}
+      {(section === SectionType.skills) && (
+        <SkillsForm />
+      )}
+      {(section === SectionType.relevantCourses) && (
+        <RelevantCoursesForm />
+      )}
+    </div>);
 }
 
 export default ResumeSections;
