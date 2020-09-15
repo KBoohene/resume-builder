9aaccd7ded162e6d0521a0f742c1e4da0a6996af
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-15 00:17:30 -0700

    Add input text to projects form

--- src/components/ResumeSections/Projects/ProjectsForm.tsx
+++ src/components/ResumeSections/Projects/ProjectsForm.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Input from '../../Shared/Input';
+import { InputType } from '../../Shared/Types'
 
 const ProjectsForm = () => {
-  return (<div></div>);
+  return (
+    <div>
+      <Input type={InputType.text} label={'Title'} value={''} placeholder={''} />
+      <Input type={InputType.textarea} label={'Description'} value={''} placeholder={''} />
+      <Input type={InputType.date} label={'From'} value={''} placeholder={''} />
+      <Input type={InputType.date} label={'To'} value={''} placeholder={''} />
+    </div>
+  );
 }
 
 export default ProjectsForm;
