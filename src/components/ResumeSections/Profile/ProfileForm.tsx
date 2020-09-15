82b8f12d73d69456b989c9abfdb7cf99e361c687
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-15 00:02:41 -0700

    Add input fields to profile form

--- src/components/ResumeSections/Profile/ProfileForm.tsx
+++ src/components/ResumeSections/Profile/ProfileForm.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Input from '../../Shared/Input';
+import { InputType } from '../../Shared/Types'
+
 
 const ProfileForm = () => {
-
-  return (<div>Done</div>);
+  return (
+    <div>
+      <Input type={InputType.text} label={'Firstname'} value={''} placeholder={''} />
+      <Input type={InputType.text} label={'Lastname'} value={''} placeholder={''} />
+      <Input type={InputType.text} label={'Email'} value={''} placeholder={''} />
+      <Input type={InputType.text} label={'LinkedIn'} value={''} placeholder={''} />
+      <div>
+        <h1>Address</h1>
+        <Input type={InputType.text} label={'Street Address'} value={''} placeholder={''} />
+        <Input type={InputType.text} label={'City'} value={''} placeholder={''} />
+        <Input type={InputType.text} label={'State'} value={''} placeholder={''} />
+        <Input type={InputType.text} label={'State'} value={''} placeholder={''} />
+        <Input type={InputType.text} label={'Apt#'} value={''} placeholder={''} />
+        <Input type={InputType.text} label={'Country'} value={''} placeholder={''} />
+      </div>
+    </div>);
 }
 
 export default ProfileForm;
