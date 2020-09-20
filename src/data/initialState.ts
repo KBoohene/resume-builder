9476528675536a193a47d04727d1a6cd26cdb2a4
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-20 16:12:35 -0700

    Change initialState from json to ts

---
+++ src/data/initialState.ts
@@ -0,0 +1,9 @@
+import { BuilderAppState } from "../reducers/ResumeReducer";
+
+export const InitialState: BuilderAppState = {
+  profile: {
+    firstName: '',
+    lastName: '',
+    email: ''
+  }
+};
