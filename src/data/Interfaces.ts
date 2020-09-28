41f242672eba2c2f5d17e7e00c8861863ad9516b
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-27 21:15:22 -0700

    Add Skill, Education, Profile

--- src/data/Interfaces.ts
+++ src/data/Interfaces.ts
@@ -1,24 +1,49 @@
-export interface BuilderAppInterface {
-  profile: {
-    firstName: string,
-    lastName: string,
-    email: string,
-    address: {
-      state: string,
-      zip: string,
-      city: string,
-      country: string,
-      street: string,
-      apt: string
-    }
-  },
-  education: {
-    schoolName: string,
+type ProjectType = {
+  title: string,
+  durationFrom: string,
+  durationTo: string,
+  description: string
+}
+
+type ProfileType = {
+  id: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  address: {
+    state: string,
+    zip: string,
     city: string,
     country: string,
-    duration: { from: string, to: string }
-  },
-  skills: [],
+    street: string,
+    apt: string
+  }
+}
+
+type EducationType = {
+  id: string,
+  schoolName: string,
+  city: string,
+  country: string,
+  duration: { from: string, to: string }
+}
+
+export type SkillType = {
+  skill: string
+}
+export interface MultipleInputInterface {
+  work: {},
+  skills: string,
+  courses: {},
+  project: ProjectType
+}
+
+export interface BuilderAppInterface {
+  profile: ProfileType,
+  education: EducationType,
+  skills: SkillType[],
   work: [],
-  courses: []
+  courses: [],
+  projects: [],
+  temp: {}
 };
