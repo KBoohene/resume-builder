2f737ab8ca11440c27d69c7c676e1b67448aa137
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-27 18:17:01 -0700

    Removed action input

--- src/components/ResumeSections/Skills/SkillsForm.tsx
+++ src/components/ResumeSections/Skills/SkillsForm.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { InputType } from '../../Shared/Types'
-import { SkillPaths } from '../../../data/paths';
-import ActionInput from '../../Shared/Components/ActionInput';
 
 const SkillsForm = () => {
   return (
     <div>
       <h2>Skills</h2>
-      <ActionInput type={InputType.text} path={SkillPaths.Skills} />
     </div>
   );
 }
--- src/components/ResumeSections/WorkExperience/WorkForm.tsx
+++ src/components/ResumeSections/WorkExperience/WorkForm.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { WorkPaths } from '../../../data/paths';
-import ActionInput from '../../Shared/Components/ActionInput';
-import { InputType } from '../../Shared/Types';
 
-// TODO: Rethink how to properly structure array and state components
 const WorkForm = () => {
   return (
     <div>
       <h1>Work Experience</h1>
 
       <h2>Description</h2>
-      <ActionInput type={InputType.textarea} path={WorkPaths.Description} />
     </div>
   );
 }
