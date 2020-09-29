a2d7210f35a947aa5709ff714c74d25682855468
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-28 22:43:15 -0700

    Modified to include multiple input reducer

--- src/components/ResumeSections/Skills/SkillsForm.tsx
+++ src/components/ResumeSections/Skills/SkillsForm.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { SkillPaths } from '../../../data/AppPaths';
+import { SkillPath, SkillsPath } from '../../../data/AppPaths';
 import MultipleInput from '../../Shared/Components/MultipleInput';
-import TempSubmit from '../../Shared/Components/TempSubmit';
+import AddItemButton from '../../Shared/Components/AddItemButton';
 import { InputType } from '../../Shared/Types';
 
 const SkillsForm = () => {
   return (
     <div>
       <h2>Skills</h2>
-      <MultipleInput type={InputType.text} label={'Skills'} value={''} placeholder={''} path={SkillPaths.Skills} />
-      <TempSubmit path={SkillPaths.Skills} />
+      <MultipleInput type={InputType.text} path={SkillPath} />
+      <AddItemButton label={'Add Item'} childPath={SkillPath} parentPath={SkillsPath} />
     </div>
   );
 }
