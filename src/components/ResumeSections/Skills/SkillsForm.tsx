c6ba2dd6e7ac88a91582d7c9634a77005e4d1f71
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-29 21:26:40 -0700

    Change Path from skills to skill

--- src/components/ResumeSections/Skills/SkillsForm.tsx
+++ src/components/ResumeSections/Skills/SkillsForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SkillPath, SkillsPath } from '../../../data/AppPaths';
+import { MultipleInputPath, SkillsPath } from '../../../data/AppPaths';
 import MultipleInput from '../../Shared/Components/MultipleInput';
 import AddItemButton from '../../Shared/Components/AddItemButton';
 import { InputType } from '../../Shared/Types';
@@ -8,10 +8,10 @@ const SkillsForm = () => {
   return (
     <div>
       <h2>Skills</h2>
-      <MultipleInput type={InputType.text} path={SkillPath} />
+      <MultipleInput type={InputType.text} path={MultipleInputPath.Skill} />
       <AddItemButton
         label={'Add Item'}
-        childPath={SkillPath}
+        childPath={MultipleInputPath.Skill}
         parentPath={SkillsPath}
       />
     </div>
