419a571e6fbb0032fcab60f2f0c354ef7424ffc4
Kwabena Boohene <kwabenaboohene@Kwabenas-MacBook-Pro.local>
2020-09-29 21:17:10 -0700

    Add project, relevant courses and work

--- src/components/ResumeBoard/ResumeBoard.tsx
+++ src/components/ResumeBoard/ResumeBoard.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import Education from './Sections/Education';
 import Profile from './Sections/Profile';
 import Skills from './Sections/Skills';
+import Course from './Sections/Course';
 import { SectionInterface, SectionType } from '../ResumeSections/Types';
+import Projects from './Sections/Projects';
+import Work from './Sections/Work';
 
-const ResumeBoard = ({ section = SectionType.skills }: SectionInterface) => {
+const ResumeBoard = ({ section = SectionType.projects }: SectionInterface) => {
   return (
     <div>
       {section === SectionType.profile && <Profile />}
       {section === SectionType.education && <Education />}
       {section === SectionType.skills && <Skills />}
-      {section === SectionType.work && <div></div>}
+      {section === SectionType.work && <Work />}
+      {section === SectionType.projects && <Projects />}
+      {section === SectionType.relevantCourses && <Course />}
     </div>
   );
 };
